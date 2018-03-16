43aff0fe167f25bf2e6a3cd90361e9be1b5e937d
soojade <soojade@sina.com>
2018-03-16 23:49:33 +0800

    完成动画

--- src/app/animations/router.animation.ts
+++ src/app/animations/router.animation.ts
@@ -1,14 +1,21 @@
 import { trigger, state, transition, style, animate } from '@angular/animations';
+import { group } from '@angular/core';
 
 export const slideToRight = trigger('routeAnimation', [
-    state('void', style({ position: 'fixed', width: '100%', height: '80%' })),
-    state('*', style({ 'position': 'fixed', 'width': '100%', 'height': '80%' })),
+    state('void', style({ 'width': '100%', 'height': '100%' })),
+    state('*', style({ 'width': '100%', 'height': '100%' })),
     transition(':enter', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('.5s ease-in-out', style({ transform: 'translateX(0)' }))
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        group([
+            animate('.5s ease-in-out', style({ transform: 'translateX(0)' })),
+            animate('.3s ease-in', style({ opacity: 1 })),
+        ])
     ]),
     transition(':leave', [
-        style({ transform: 'translateX(0' }),
-        animate('.5s ease-in-out', style({ transform: 'translateX(100%)' }))
-    ])
+        style({ transform: 'translateX(0)', opacity: 1 }),
+        group([
+            animate('.5s ease-in-out', style({ transform: 'translateX(100%)' })),
+            animate('.3s ease-in', style({ opacity: 0 })),
+        ])
+    ]),
 ]);
--- src/app/project/project-list/project-list.component.ts
+++ src/app/project/project-list/project-list.component.ts
@@ -4,20 +4,23 @@ import { NewProjectComponent } from '../new-project/new-project.component';
 import { InviteComponent } from '../invite/invite.component';
 import { ConfirmDialogComponent } from '../../shared/confirm-dialog/confirm-dialog.component';
 import { slideToRight } from '../../animations/router.animation';
+import { listAnimation } from '../../animations/list.animation';
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
   styleUrls: ['./project-list.component.scss'],
-  animations: [slideToRight]
+  animations: [slideToRight, listAnimation]
 })
 export class ProjectListComponent implements OnInit {
   projects = [
     {
+      "id": 1,
       "name": "协作平台",
       "desc": "内部项目",
       "coverImg": "assets/img/covers/0.jpg"
     },
     {
+      "id": 2,
       "name": "测试项目",
       "desc": "测试描述",
       "coverImg": "assets/img/covers/1.jpg"
@@ -30,15 +33,21 @@ export class ProjectListComponent implements OnInit {
   }
   openNewProjectDialog() {
     const dialogRef = this.dialog.open(NewProjectComponent, { data: { title: '新建项目' } }); // 获取对话框引用
-    // dialogRef.afterClosed().subscribe(); // 订阅对话框引用可观察对象,使调用者接收对话框消息
+    dialogRef.afterClosed().subscribe(() => {
+      this.projects = [...this.projects, { 'id': 3, 'name': 'linshi', 'desc': 'hello', "coverImg": "assets/img/covers/1.jpg" }];
+    }); // 订阅对话框引用可观察对象,使调用者接收对话框消息
   }
+
   openInviteDialog() {
     this.dialog.open(InviteComponent);
   }
   edit() {
     const dialogRef = this.dialog.open(NewProjectComponent, { data: { title: '编辑项目' } });
   }
-  openConfirmDialog() {
+  openConfirmDialog(project) {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { title: '删除项目', content: '确认是否要删除?' } });
+    dialogRef.afterClosed().subscribe(() => {
+      this.projects = this.projects.filter(p => p.id != project.id);
+    })
   }
 }
